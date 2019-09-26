306204b3d5840b5b8685cafb7c143b1f55261de9
Ilya Zorin <geraltencore@gmail.com>
2019-09-26 23:14:25 +0100

    Strings

--- compiler.c
+++ compiler.c
@@ -3,6 +3,7 @@
 
 #include "common.h"
 #include "compiler.h"
+#include "object.h"
 #include "scanner.h"
 #include "vm.h"
 
@@ -200,6 +201,11 @@ static void number() {
   emitConstant(NUMBER_VAL(value));
 }
 
+static void string() {
+  emitConstant(OBJ_VAL(
+      copyString(parser.previous.start + 1, parser.previous.length - 2)));
+}
+
 static void unary() {
   TokenType operatorType = parser.previous.type;
 
@@ -240,7 +246,7 @@ ParseRule rules[] = {
     {NULL, binary, PREC_COMPARISON}, // TOKEN_LESS
     {NULL, binary, PREC_COMPARISON}, // TOKEN_LESS_EQUAL
     {NULL, NULL, PREC_NONE},         // TOKEN_IDENTIFIER
-    {NULL, NULL, PREC_NONE},         // TOKEN_STRING
+    {string, NULL, PREC_NONE},       // TOKEN_STRING
     {number, NULL, PREC_NONE},       // TOKEN_NUMBER
     {NULL, NULL, PREC_NONE},         // TOKEN_AND
     {NULL, NULL, PREC_NONE},         // TOKEN_CLASS
