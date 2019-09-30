201eb8df3113c0f8a1d3adab1dc175ea0d49eabf
Ilya Zorin <geraltencore@gmail.com>
2019-09-30 22:58:57 +0100

    Global Variables

--- vm.c
+++ vm.c
@@ -10,7 +10,9 @@
 
 VM vm;
 
-static void resetStack() { vm.stackTop = vm.stack; }
+static void resetStack() {
+  vm.stackTop = vm.stack;
+}
 
 static void runtimeError(const char *format, ...) {
   va_list args;
@@ -29,11 +31,13 @@ static void runtimeError(const char *format, ...) {
 void initVM() {
   resetStack();
   vm.objects = NULL;
+  initTable(&vm.globals);
   initTable(&vm.strings);
 }
 
 void freeVM() {
   freeObjects();
+  freeTable(&vm.globals);
   freeTable(&vm.strings);
 }
 
@@ -47,7 +51,9 @@ Value pop() {
   return *vm.stackTop;
 }
 
-static Value peek(int distance) { return vm.stackTop[-1 - distance]; }
+static Value peek(int distance) {
+  return vm.stackTop[-1 - distance];
+}
 
 static bool isFalsey(Value value) {
   return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
@@ -70,6 +76,7 @@ static void concatenate() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define READ_STRING() AS_STRING(READ_CONSTANT())
 
 #define BINARY_OP(valueType, op)                                               \
   do {                                                                         \
@@ -110,6 +117,34 @@ static InterpretResult run() {
     case OP_FALSE:
       push(BOOL_VAL(false));
       break;
+    case OP_POP:
+      pop();
+      break;
+    case OP_GET_GLOBAL: {
+      ObjString *name = READ_STRING();
+      Value value;
+      if (!tableGet(&vm.globals, name, &value)) {
+        runtimeError("Undefined variable '%s'.", name->chars);
+        return INTERPRET_RUNTIME_ERROR;
+      }
+      push(value);
+      break;
+    }
+    case OP_DEFINE_GLOBAL: {
+      ObjString *name = READ_STRING();
+      tableSet(&vm.globals, name, peek(0));
+      pop();
+      break;
+    }
+    case OP_SET_GLOBAL: {
+      ObjString *name = READ_STRING();
+      if (tableSet(&vm.globals, name, peek(0))) {
+        tableDelete(&vm.globals, name);
+        runtimeError("Undefined variable '%s'.", name->chars);
+        return INTERPRET_RUNTIME_ERROR;
+      }
+      break;
+    }
     case OP_EQUAL: {
       Value b = pop();
       Value a = pop();
@@ -155,15 +190,19 @@ static InterpretResult run() {
 
       push(NUMBER_VAL(-AS_NUMBER(pop())));
       break;
-    case OP_RETURN:
+    case OP_PRINT:
       printValue(pop());
       printf("\n");
+      break;
+    case OP_RETURN:
+      // Exit interpreter.
       return INTERPRET_OK;
     }
   }
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef READ_STRING
 #undef BINARY_OP
 }
 
